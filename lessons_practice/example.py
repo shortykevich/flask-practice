a7ab1c6e5176bdea23fbe595d82d9cff13dc9f79
Kirill Dvoretsky <shortykofficial@gmail.com>
2024-07-29 23:40:48 +0300

    Learned how to indicate HTTP request methods in Flask in two ways

--- lessons_practice/example.py
+++ lessons_practice/example.py
@@ -1,9 +1,26 @@
 from flask import Flask
+from flask import request
 
 # Это callable WSGI-приложение
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Oh shit, i\'m sorry'
+def index():
+    return 'Hello, World!'
+
+# 1st example of methods indication:
+# @app.get('/users')
+# def users_get():
+#     return 'GET /users'
+#
+#
+# @app.post('/users')
+# def users_post():
+#     return 'POST /users'
+
+
+# 2nd example of methods indication:
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    return f'Hello from {request.method} /users'
