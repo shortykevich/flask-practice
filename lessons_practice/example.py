1ca46247d4112ed7965ccf189a3cdb98d1f00418
Kirill Dvoretsky <shortykofficial@gmail.com>
2024-07-30 02:51:59 +0300

    Learned basics of rerquest-response sessions and how to work with request args

--- lessons_practice/example.py
+++ lessons_practice/example.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from lessons_practice.data import generate_companies
 
-# Это callable WSGI-приложение
+
 app = Flask(__name__)
+companies = generate_companies(100)
 
 
 @app.route('/')
-def hello_world():
-    return 'Oh shit, i\'m sorry'
+def index():
+    return "<a href='/companies'>Companies</a>"
+
+
+@app.route('/companies')
+def get_companies():
+    page = request.args.get('page', default=1, type=int)
+    per = request.args.get('per', default=5, type=int)
+    target_page = ((page - 1) * per)
+    return jsonify(companies[target_page:target_page + per])
+
