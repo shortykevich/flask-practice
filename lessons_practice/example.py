ef2540e5f7258af669625e76f4c3f7848d13f1c1
Kirill Dvoretsky <shortykofficial@gmail.com>
2024-07-30 20:37:48 +0300

    Learned basics of jinja template engine

--- lessons_practice/example.py
+++ lessons_practice/example.py
@@ -1,9 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
-# Это callable WSGI-приложение
+from lessons_practice.data import generate_users
+
+users = generate_users(100)
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Oh shit, i\'m sorry'
+def index():
+    return render_template('index.html')
+
+
+# BEGIN (write your solution here)
+@app.route('/users')
+def get_users():
+    return render_template(
+        'users/index.html',
+        users=users,
+    )
+
+
+@app.route('/users/<int:id>')
+def show_user(id):
+    filtered_users = filter(lambda user: user['id'] == id, users)
+    found_user = next(filtered_users, None)
+
+    if found_user is None:
+        return 'Page not found', 404
+    return render_template(
+        'users/show.html',
+        user=dict(found_user),
+    )
+# END
