88e3bdcb5adb7650bad7a1a24cd0e4b8ad2c5b98
Kirill Dvoretsky <shortykofficial@gmail.com>
2024-07-30 03:46:46 +0300

    Learned about dynamic routes and placeholders

---
+++ lessons_practice/data.py
@@ -0,0 +1,25 @@
+import random
+
+from faker import Faker
+
+SEED = 1234
+
+
+def generate_companies(companies_count):
+    fake = Faker()
+    fake.seed_instance(SEED)
+
+    ids = list(range(companies_count))
+    random.seed(SEED)
+    random.shuffle(ids)
+
+    companies = []
+
+    for i in range(companies_count):
+        companies.append({
+            "id": ids[i],
+            "name": fake.company(),
+            "phone": fake.phone_number(),
+        })
+
+    return companies
--- lessons_practice/example.py
+++ lessons_practice/example.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 
-# Это callable WSGI-приложение
+from lessons_practice.data import generate_companies
+
+companies = generate_companies(100)
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Oh shit, i\'m sorry'
+def index():
+    return 'open something like (you can change id): /companies/5'
+
+
+@app.route('/companies/<int:id>')
+def get_company(id):
+    found_company = [company for company in companies if company['id'] == id]
+    if found_company:
+        return jsonify(found_company[0])
+    return 'Page not found', 404
