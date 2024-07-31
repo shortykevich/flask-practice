a17c9953908f55b96e0a6d583e2f5f97fe050225
Kirill Dvoretsky <shortykofficial@gmail.com>
2024-08-01 02:04:18 +0300

    Learned what is CRUD, made index and show routes

--- lessons_practice/example.py
+++ lessons_practice/example.py
@@ -1,9 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from lessons_practice.repository import PostsRepository
 
-# Это callable WSGI-приложение
 app = Flask(__name__)
+
+repo = PostsRepository(50)
 
 
 @app.route('/')
-def hello_world():
-    return 'Oh shit, i\'m sorry'
+def index():
+    return render_template('index.html')
+
+
+# BEGIN (write your solution here)
+@app.route('/posts/<slug>')
+def show_post(slug):
+    post = repo.find(slug)
+
+    if not post:
+        return 'Page not found', 404
+
+    return render_template(
+        'posts/show.html',
+        post=post
+    )
+
+
+@app.route('/posts')
+def show_posts():
+    posts = repo.content()
+    page = request.args.get('page', default=1, type=int)
+    per_page = 5
+
+    target_page = ((page - 1) * per_page)
+    next_page = page if page == len(posts) // per_page else page + 1
+    prev_page = page if page == 1 else page - 1
+    return render_template(
+        'posts/index.html',
+        posts=posts[target_page:target_page + per_page],
+        next_page=next_page,
+        prev_page=prev_page
+    )
+# END
